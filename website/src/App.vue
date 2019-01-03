9d9ae5b08fa09184a04f1b521e5198db995f96b4
José Luis Santana <yuniorglez@MacBook-Pro-de-Jose.local>
2019-01-03 22:48:33 +0000

    Esta sería la estructura base.
    Se ha generado un proyecto de vue, se ha instalado bootstrap y axios.
    
    Se crea el componente Home que prepara la base con un for y una computed

---
+++ website/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ website/src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <b-container class="hello">
+    <h1>Busca los recursos que necesites</h1>
+    <small>No le busques sentido al taco</small>
+    <b-row>
+      <b-input-group>
+        <b-input-group-text slot="prepend">
+          <strong class="text-danger">🌮</strong>
+        </b-input-group-text>
+        <b-form-input v-model="searching"></b-form-input>
+      </b-input-group>
+    </b-row>
+
+    <b-row>
+      <b-card
+        v-for="card in results"
+        :title="card.title || 'Título del curso'"
+        :img-src="card.image || 'https://placehold.it/100x100'"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-button :href="card.href" variant="primary">Ver curso</b-button>
+      </b-card>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      typeOfResources: ["Libros", "Papers", "Proyectos"],
+      searching: "",
+      allData: []
+    };
+  },
+  computed: {
+    results() {
+      return ["resultado"];
+    }
+  },
+  created() {
+    console.log("hi");
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
